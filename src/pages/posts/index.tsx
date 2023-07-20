6fe54636d070441b6d4845840a2d38b802a7a66e
JA-Lourenco <ja.lourenco@outlook.com>
2023-07-19 21:10:02 -0300

    Page: styles for navigation buttons

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,6 +2,13 @@ import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
 
+import {
+  FiChevronLeft,
+  FiChevronsLeft,
+  FiChevronRight,
+  FiChevronsRight,
+} from "react-icons/fi";
+
 import thumb from "../../../public/images/reactStartPost.png";
 
 import styles from "./styles.module.scss";
@@ -27,6 +34,28 @@ export default function Posts() {
               exercitationem molestias non.
             </p>
           </Link>
+
+          <div className={styles.navigation}>
+            <div>
+              <button>
+                <FiChevronsLeft size={25} color="var(--white)" />
+              </button>
+
+              <button>
+                <FiChevronLeft size={25} color="var(--white)" />
+              </button>
+            </div>
+
+            <div>
+              <button>
+                <FiChevronsRight size={25} color="var(--white)" />
+              </button>
+
+              <button>
+                <FiChevronRight size={25} color="var(--white)" />
+              </button>
+            </div>
+          </div>
         </div>
       </main>
     </>
